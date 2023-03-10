6e1dd7bfa48f0d7ffcef092a352f5d9e3f07d711
nkowne63rt <neutron63zf@gmail.com>
2023-03-10 23:47:34 +0900

    add: search agent sample

---
+++ main.py
@@ -0,0 +1,23 @@
+from langchain.agents import initialize_agent
+from langchain.agents import load_tools
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+from langchain.llms import OpenAI
+from dotenv import load_dotenv
+load_dotenv(override=True)
+
+
+# First, let's load the language model we're going to use to control the agent.
+llm = OpenAI(temperature=0)
+
+# Next, let's load some tools to use. Note that the `llm-math` tool uses an LLM, so we need to pass that in.
+tools = load_tools(["serpapi", "llm-math"], llm=llm)
+
+
+# Finally, let's initialize an agent with the tools, the language model, and the type of agent we want to use.
+agent = initialize_agent(
+    tools, llm, agent="zero-shot-react-description", verbose=True)
+
+# Now let's test it out!
+agent.run(
+    "Who is Olivia Wilde's boyfriend? What is his current age raised to the 0.23 power?")
